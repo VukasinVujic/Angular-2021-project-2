6a6ae2478b952643166f0079952dc62c539163bc
vukasinvujic <vukasinvujic021@gmail.com>
2021-09-03 07:29:19 +0200

    shpping list

---
+++ projectTWO/src/app/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  //   public name: string;
+  //   public amount: number;
+
+  constructor(public name: string, public amount: number) {
+    // same affect as declaring and adding values
+    //***
+    // this.name = name;
+    // this.amount = amount;
+  }
+}
