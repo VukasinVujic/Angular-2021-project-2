cff94098ba10d3656b471698a304aecbba781d0c
vukasinvujic <vukasinvujic021@gmail.com>
2021-10-02 12:50:33 +0200

    adding logic to rec service and removing the same from .ts & html

--- projectTWO/src/app/recipes/recipe-list/recipe-list.component.ts
+++ projectTWO/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -9,12 +9,6 @@ import { RecipeService } from '../recipe.service';
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
-
-  @Output() recipeWasSelected = new EventEmitter<Recipe>();
-
-  onRecipeSelected(recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe);
-  }
 
   constructor(private recipeService: RecipeService) {}
 
